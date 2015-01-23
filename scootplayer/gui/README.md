af0661132bbd174ea7fc0a2c668a42f295b7ab70
Sean Baildon <sean@sbaildon.com>
2015-01-23 15:55:49 +0000

    Remove logic from GUI
    Refactored code to bring logic from GUI to Flask, basic options for the GUI, ability to toggle graphs on/off

---
+++ scootplayer/gui/README.md
@@ -0,0 +1,14 @@
+## GUI
+
+Start Scootplayer as normal, and point a web browser to 127.0.0.1:5000
+
+### Config
+
+To configure the GUI, you will need to edit `gui_config.yaml` in the Scootplayer top level directory.
+
+#### Options
+
+`display: [true/false]` 
+
+Enable and disable visbility for each metric.
+
--- scootplayer/gui/static/graphs.js
+++ scootplayer/gui/static/graphs.js
@@ -1,40 +1,20 @@
-function request_time_series_data(buffer, metric) {
+function request_graphing_data(buffer) {
+    if($('#' + buffer + '_graphs').is(':hidden')) {
+        return;
+    }
     $.ajax({
-        url: buffer + '/' + metric,
+        url: buffer
     }).success(function(data) {
-        data_series = [];
-        for (var i = 0; i < data[metric].length; i++) {
-            data_series.push([data['time_elapsed'][i], data[metric][i]]);
+        for (metric in data) {
+            $.plot($("#" + buffer + '-' + metric), [ data[metric] ]);
         }
-        $.plot($("#" + buffer + '-' + metric), [ data_series ]);
     });
 
 }
 
-function test2() {
-    request_time_series_data('playback', 'time_buffer');
-    request_time_series_data('playback', 'url_bitrate');
-    request_time_series_data('playback', 'time_position');
-    request_time_series_data('playback', 'bandwidth');
-    request_time_series_data('playback', 'moving_average_occupancy');
-    request_time_series_data('playback', 'max_encoded_bitrate');
-    request_time_series_data('playback', 'id');
-    request_time_series_data('playback', 'moving_average_bandwidth');
-
-    request_time_series_data('download', 'time_buffer');
-    request_time_series_data('download', 'url_bitrate');
-    request_time_series_data('download', 'time_position');
-    request_time_series_data('download', 'bandwidth');
-    request_time_series_data('download', 'moving_average_occupancy');
-    request_time_series_data('download', 'max_encoded_bitrate');
-    request_time_series_data('download', 'id');
-    request_time_series_data('download', 'moving_average_bandwidth');
+function plot_graphs() {
+    request_graphing_data('playback');
+    request_graphing_data('download');
 }
 
-var interval = setInterval(test2, 1000);
-
-
-
-// window.onresize = function(event) {
-//     $.plot($("#moving_average_bandwidth"), [ [[0, 0], [1, 1]] ], { yaxis: { max: 1 } });
-// }
+var interval = setInterval(plot_graphs, 1000);
