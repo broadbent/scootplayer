555e400ce608284cea3a54eb2bd51dfeca9a54a3
Sean Baildon <sean@sbaildon.com>
2015-01-23 15:56:29 +0000

    include gui/web.py

--- scootplayer/gui/web.py
+++ scootplayer/gui/web.py
@@ -2,30 +2,65 @@ from flask import Flask, render_template, jsonify
 import threading
 import time
 import flot
+import pprint
+import ConfigParser
+import yaml
 
 app = Flask(__name__)
-report = {'playback': {}, 'download': {}}
+report = { 'playback': {}, 'download': {} }
+graphing_data = { 'playback': {}, 'download': {} }
 
 @app.route('/')
 def homepage():
-    return render_template('index.html', playback=report['playback'], download=report['download'])
+    return render_template('index.html', playback=graphing_data['playback'], download=graphing_data['download'])
+
+@app.route('/<buffer_>')
+def return_graphing_data(buffer_):
+    return jsonify(graphing_data[buffer_])
 
 @app.route('/<buffer_>/<metric>')
-def data(buffer_, metric):
+def metric_data(buffer_, metric):
     return jsonify({metric: report[buffer_][metric], 'time_elapsed': report[buffer_]['time_elapsed']})
 
-def send_data(obj, data):
-    if obj not in report:
+def send_data(buffer_, data):
+    if buffer_ not in report:
         value = {}
-        for element in data:
-            value[element] = [data[element]]
-        report[obj] = value
+        for metric in data:
+            value[metric] = [data[metric]]
+        report[buffer_] = value
     else:
-        for element in data:
+        for metric in data:
             try:
-                report[obj][element].append(data[element])
+                report[buffer_][metric].append(data[metric])
             except KeyError:
-                report[obj][element] = [data[element]]
+                report[buffer_][metric] = [data[metric]]
+    prepare_graphing_data(buffer_)
+
+def prepare_graphing_data(buffer_):
+    for metric in report[buffer_]:
+        if metric not in graphs_to_display[buffer_]:
+            continue
+        temp_array = []
+        for idx, val in enumerate(report[buffer_][metric]):
+            temp_array.append((report[buffer_]['time_elapsed'][idx], report[buffer_][metric][idx]))
+        graphing_data[buffer_][metric] = temp_array
+
+def get_graphs_to_display(config):
+    graphs_to_display = {}
+    for buffer_ in config:
+        graphs_to_display[buffer_] = []
+        for metric in config[buffer_]:
+            if config[buffer_][metric]['display']:
+                graphs_to_display[buffer_].append(metric)
+
+    return graphs_to_display
+
+def parse_config():
+    stream = open("gui_config.yaml", 'r')
+    return yaml.load(stream)
+
+config = parse_config()
+graphs_to_display = get_graphs_to_display(config)
 
 class webserver_thread(threading.Thread):
     def __init__(self):
